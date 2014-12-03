d794eab0ae552f7d0379302b2eedf33340882b56
Aliaksandr Dubrouski <dubrousky@gmail.com>
2014-12-03 22:40:42 +0300

    Text field with browse button was replaced by the standard com.intellij.openapi.ui class.

--- src/repl/simple/mathematica/Ui/ConfigCenterPanel.java
+++ src/repl/simple/mathematica/Ui/ConfigCenterPanel.java
@@ -1,13 +1,14 @@
 package repl.simple.mathematica.Ui;
 
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.ui.TextBrowseFolderListener;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.uiDesigner.core.Spacer;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 
 /**
@@ -15,59 +16,17 @@ import java.awt.event.ActionListener;
  */
 public class ConfigCenterPanel {
     JPanel rootPanel;
-    JTextField mathLinkPath;
-    JTextField mathKernelPath;
-    JTextField nativeLibPath;
+    TextFieldWithBrowseButton mathLinkPath;
+    TextFieldWithBrowseButton mathKernelPath;
+    TextFieldWithBrowseButton nativeLibPath;
 
-    JButton button1;
-    JButton button2;
-    JButton button3;
+
     JTextField mathLinkArgs;
 
     public ConfigCenterPanel() {
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                // TODO: show file selection dialog and update mathLinkPath
-                // LocalFileSystem.getInstance().findFileByIoFile(new File("/"))
-                Frame f = new Frame();
-                f.setSize(500, 500);
-                // get path for JLink java library
-                FileDialog fd = new FileDialog(f, "Full path to JLink.jar", FileDialog.LOAD);
-                fd.setDirectory("/");
-                fd.setFile("*.jar");
-                fd.setVisible(true);
-                mathLinkPath.setText(fd.getFile());
-            }
-        });
-        button2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                // TODO: show file selection dialog and update mathKernelPath
-                Frame f = new Frame();
-                f.setSize(500, 500);
-                // get path for JLink java library
-                FileDialog fd = new FileDialog(f, "Full path to JLink.jar", FileDialog.LOAD);
-                fd.setDirectory("/");
-                fd.setFile("*.jar");
-                fd.setVisible(true);
-                mathKernelPath.setText(fd.getFile());
-            }
-        });
-        button3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                // TODO: show file selection dialog and update nativeLibPath
-                Frame f = new Frame();
-                f.setSize(500, 500);
-                // get path for JLink java library
-                FileDialog fd = new FileDialog(f, "Full path to JLink.jar", FileDialog.LOAD);
-                fd.setDirectory("/");
-                fd.setFile("*.jar");
-                fd.setVisible(true);
-                nativeLibPath.setText(fd.getDirectory());
-            }
-        });
+        mathLinkPath.addBrowseFolderListener(new TextBrowseFolderListener(new FileChooserDescriptor(true, false, true, false, false, false)));
+        mathKernelPath.addBrowseFolderListener(new TextBrowseFolderListener(new FileChooserDescriptor(true, false, false, false, false, false)));
+        nativeLibPath.addBrowseFolderListener(new TextBrowseFolderListener(new FileChooserDescriptor(false, true, false, false, false, false)));
     }
 
     public JPanel getRootPanel() {
@@ -122,7 +81,7 @@ public class ConfigCenterPanel {
      */
     private void $$$setupUI$$$() {
         rootPanel = new JPanel();
-        rootPanel.setLayout(new GridLayoutManager(5, 4, new Insets(0, 0, 0, 0), -1, -1));
+        rootPanel.setLayout(new GridLayoutManager(5, 3, new Insets(0, 0, 0, 0), -1, -1));
         final JLabel label1 = new JLabel();
         label1.setText("MathLink Path");
         rootPanel.add(label1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
@@ -132,24 +91,15 @@ public class ConfigCenterPanel {
         final JLabel label3 = new JLabel();
         label3.setText("Native Library Path");
         rootPanel.add(label3, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        mathLinkPath = new JTextField();
+        mathLinkPath = new TextFieldWithBrowseButton();
         mathLinkPath.setText("");
         rootPanel.add(mathLinkPath, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-        mathKernelPath = new JTextField();
+        mathKernelPath = new TextFieldWithBrowseButton();
         rootPanel.add(mathKernelPath, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-        nativeLibPath = new JTextField();
+        nativeLibPath = new TextFieldWithBrowseButton();
         rootPanel.add(nativeLibPath, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-        button1 = new JButton();
-        button1.setText("...");
-        rootPanel.add(button1, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        button2 = new JButton();
-        button2.setText("...");
-        rootPanel.add(button2, new GridConstraints(1, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        button3 = new JButton();
-        button3.setText("...");
-        rootPanel.add(button3, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer1 = new Spacer();
-        rootPanel.add(spacer1, new GridConstraints(0, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        rootPanel.add(spacer1, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final Spacer spacer2 = new Spacer();
         rootPanel.add(spacer2, new GridConstraints(4, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         final JLabel label4 = new JLabel();
