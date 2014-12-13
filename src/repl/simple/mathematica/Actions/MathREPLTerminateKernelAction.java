1f26e19c219c6914de7ce7c7edbb51637e5e8cb4
Aliaksandr Dubrouski <dubrousky@gmail.com>
2014-12-14 00:06:33 +0300

    Added confirmation dialog before terminating session.

--- src/repl/simple/mathematica/Actions/MathREPLTerminateKernelAction.java
+++ src/repl/simple/mathematica/Actions/MathREPLTerminateKernelAction.java
@@ -21,9 +21,12 @@ import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.vcs.VcsShowConfirmationOption;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.ui.content.Content;
+import com.intellij.util.ui.ConfirmationDialog;
+import com.intellij.util.ui.DialogUtil;
 import repl.simple.mathematica.MathREPLBundle;
 import repl.simple.mathematica.MathSessionWrapper;
 
@@ -31,15 +34,7 @@ import java.lang.reflect.InvocationTargetException;
 
 
 /**
-<<<<<<< HEAD
-<<<<<<< HEAD
- * Terminates the Kernel session
-=======
  * Terminates the kernel running in the active tab
->>>>>>> 8ebc6dc... Cleanup code and add rework the actions. Added storage for plugin icons.
-=======
- * Terminates the kernel running in the active tab
->>>>>>> 34511724236e01d14c34e4ceb016169aa912577c
  */
 public class MathREPLTerminateKernelAction extends MathREPLKernelAction {
     public MathREPLTerminateKernelAction() {
@@ -68,12 +63,20 @@ public class MathREPLTerminateKernelAction extends MathREPLKernelAction {
         ToolWindow tw = twm.getToolWindow(TOOL_WINDOW);
         final MathSessionWrapper msw =  MathSessionWrapper.adopt(tw.getContentManager().getSelectedContent().getComponent());
         try {
-            msw.call("closeLink");
-            Sessions.put(tw.getContentManager().getSelectedContent().getTabName(),true);
-            new Notification("REPL",
-                    "JLink",
-                    MathREPLBundle.message("kernelStopped"),
-                    NotificationType.INFORMATION).notify( e.getProject() );
+            // Confirm the action
+            if( ConfirmationDialog.requestForConfirmation(VcsShowConfirmationOption.STATIC_SHOW_CONFIRMATION,
+                    e.getProject(),
+                    MathREPLBundle.message("confirmTerminate"),
+                    "Confirm Dialog",
+                    null) )
+            {
+                msw.call("closeLink");
+                Sessions.put(tw.getContentManager().getSelectedContent().getTabName(), true);
+                new Notification("REPL",
+                        "JLink",
+                        MathREPLBundle.message("kernelStopped"),
+                        NotificationType.INFORMATION).notify(e.getProject());
+            }
             // Change Toolbar appearance (name)
         } catch (NoSuchMethodException e1) {
             e1.printStackTrace();
